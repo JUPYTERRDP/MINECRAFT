68f3a1c31f2fa2b3ca4bac45cab71b60c0109460
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-05-03 11:06:39 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Create privilege separation directory for SSH
-mkdir -p /run/sshd
+# Create the user account if it doesn't exist
+if ! id -u Albin &>/dev/null; then
+    echo "Creating user Albin..."
+    useradd -m Albin
+fi
 
-# Start SSH service
-/usr/sbin/sshd
+# Set password for the user
+echo "Setting password for user Albin..."
+echo "Albin:Albin4242" | chpasswd
 
 # Start Chrome Remote Desktop host with custom command
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYdu7up_U8uzR5FWgxEmTUBofOG9xpZw9TmqIqVzXxXAum37mTqdlD6_Mb5waEJhdw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name=Albin
