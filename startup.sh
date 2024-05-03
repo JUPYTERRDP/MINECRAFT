22cda19b44cf99c79a15b575476f1b9ae83c0f4a
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-05-03 10:34:10 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Set the display environment variable
-export DISPLAY=:0
+# Create privilege separation directory for SSH
+mkdir -p /run/sshd
 
-# Start Chrome Remote Desktop host with your custom command
-/opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYf7MUrqTY8Tohk-R3mwFUVetcw-xbYl5J05VhVmYGC2KP8f-TLQ5Tmgk6T9lBFdTQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) &
+# Start SSH service
+/usr/sbin/sshd
 
-# Start SSH (if needed)
-/usr/sbin/sshd -D
+# Start Chrome Remote Desktop host with custom command
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYdu7up_U8uzR5FWgxEmTUBofOG9xpZw9TmqIqVzXxXAum37mTqdlD6_Mb5waEJhdw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
