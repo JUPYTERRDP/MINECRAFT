67b5d4d20b2026e126458bec2c919c3963509031
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-05-03 08:30:35 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 export DISPLAY=:0
 
 # Start Chrome Remote Desktop host with your custom command
-/opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHAPxtPXPhRvlypLcbOku7DoB0hewui3FLVdNz89tJCX1NVGIX1S7IMJc4RKgn_vvg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) &
+/opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYf7MUrqTY8Tohk-R3mwFUVetcw-xbYl5J05VhVmYGC2KP8f-TLQ5Tmgk6T9lBFdTQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) &
 
 # Start SSH (if needed)
 /usr/sbin/sshd -D
