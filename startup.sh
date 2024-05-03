c4897b79c5e63947655c1d91c86c5882915a9708
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-05-03 16:50:48 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ fi
 
 # Set password for the user
 echo "Setting password for user Albin..."
-echo "Albin:Albin4242" | chpasswd
+echo "Albin:Albin4242" | passwd Albin --stdin
 
 # Start Chrome Remote Desktop host with custom command
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYdx_6GkV6PzhX-d-3_wXj2uNdSrzSIDU1_ORJgtc67LjTzyrZXZpb4deWpa-vfgtA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name=Albin
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYdqgI_ojgmugpSpfb0hXTG0pf-0K4GyO9VNqs6hwHFFCaeiRSGZpTWk3x2TQ8ppQA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --user-name=Albin
